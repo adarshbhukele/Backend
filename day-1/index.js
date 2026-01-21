e0a50c9cb08eac61f5ee7160b5f60caac294f86f
adarshbhukele <aadarshbhukele2005@gmail.com>
2026-01-21 15:26:49 +0530

    this is my first program

--- day-1/index.js
+++ day-1/index.js
@@ -5,4 +5,12 @@ app.get("/",(req,res)=>{
     res.send("hello this is my first program")
 })
 
+app.get("/about",(req,res)=>{
+    res.send("This is about page")
+})
+
+app.get("/home",(req,res)=>{
+    res.send("This is Home Page")
+})
+
 app.listen(3000)
